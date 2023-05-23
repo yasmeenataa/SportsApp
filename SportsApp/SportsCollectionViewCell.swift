e02dda91b0e60de9910c8324f8ff9346f91d7632
yasmeenataa <yasmenataa7@gmail.com>
2023-05-23 23:49:12 +0300

    design all main screans

---
+++ SportsApp/SportsCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  SportsCollectionViewCell.swift
+//  SportsApp
+//
+//  Created by Mac on 23/05/2023.
+//
+
+import UIKit
+
+class SportsCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var sportName: UILabel!
+    @IBOutlet weak var sportImage: UIImageView!
+}
---
+++ SportsApp/SportsController.swift
@@ -0,0 +1,46 @@
+//
+//  SportsController.swift
+//  SportsApp
+//
+//  Created by Mac on 23/05/2023.
+//
+
+import UIKit
+
+class SportsController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+   
+    
+
+    @IBOutlet weak var sportsCollection: UICollectionView!
+    var sportsArray : [String] = ["Football", "Basketball", "Cricket", "Tennis"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        sportsCollection.dataSource = self
+        sportsCollection.delegate = self
+        self.navigationItem.title = "Sports"
+        
+      
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width:  (UIScreen.main.bounds.size.width/2 - 25), height: 180)
+    }
+    
+    
+
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return sportsArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? SportsCollectionViewCell
+        cell?.sportImage.image = UIImage(named: sportsArray[indexPath.row])
+        cell?.sportName.text = sportsArray[indexPath.row]
+        return cell!
+    }
+
+}
