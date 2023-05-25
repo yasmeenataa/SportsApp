e819e6c59dd9d01dab30e1c5a51082cdc8b2bd42
yasmeenataa <yasmenataa7@gmail.com>
2023-05-25 09:06:42 +0300

    create model for LocalLeagues to use it in Core Data

---
+++ SportsApp/Model/LocalLeague.swift
@@ -0,0 +1,16 @@
+//
+//  LocalLeague.swift
+//  SportsApp
+//
+//  Created by Mac on 25/05/2023.
+//
+
+import Foundation
+struct LocalLeague{
+    let leagueKey: Int
+    let leagueName: String
+    let sportName : String
+    let leagueLogo: String
+   
+   
+}
